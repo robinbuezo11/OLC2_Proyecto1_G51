87850f0f299d0cebd6c69f7bbca61520b95d1416
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2023-12-14 01:10:48 -0600

    [update] front - monaco editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Reports from './pages/Reports';
 
 
 function App() {
@@ -10,8 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/reports" element={<Reports/>} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,84 +1,140 @@
 import '../styles/Dashboard.css';
-import Button from 'react-bootstrap/Button';
+import  { useState, useRef} from 'react';
+import { saveAs } from 'file-saver';
+import Editor from '@monaco-editor/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
-import MonacoEditor from 'react-monaco-editor';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
-
-function Dashboard() {
+function App() {
   const [code, setCode] = useState('');
-  const [result, setResult] = useState('');
-  const hiddenFileInput = React.useRef(null);
+  const [resultado, setResultado] = useState('');
+  const navDropdownRef = useRef(null);
 
-  function executeCommand(command) {
-    axios.post('http://18.117.141.89:8000/api/command', {
-      command: command
-    }).then((response) => {
-      if(response.data.status === 'success') {
-        setResult(response.data.result);
-      } else {
-        setResult(response.data.error);
+  const options = {
+  };
+
+  function analizar(){
+    axios.post('http://localhost:5000/analyzer', {
+      input: code
+    })
+    .then(function (response) {
+      console.log(response);
+      if(response.data.errors.length > 0){
+        setResultado(response.data.result + '\n' + '--- Se encontraron errores en el código ---');
+      }else{
+        setResultado(response.data.result);
       }
-    }).catch((error) => {
-      alert(error);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
   }
 
-  function handleChange(e) {
+  function graphAST(){
+    axios.get('http://localhost:5000/graphAST')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  function generateSymbols(){
+    axios.get('http://localhost:5000/generateSymbols')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  function generateErrors(){
+    axios.get('http://localhost:5000/generateErrors')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  function open(){
+    let win = window.open('http://localhost:3000/', '_blank');
+    win.focus();
+  }
+
+  function save(){
+    const blob = new Blob([code], {type: 'text/plain;charset=utf-8'});
+    saveAs(blob, 'file.tw');
+  }
+
+  function read(e){
     const file = e.target.files[0];
     if (!file) return;
-    const reader = new FileReader();
+    const fileReader = new FileReader();
 
-    reader.readAsText(file);
-    reader.onload = () => {
-      setCode(reader.result);
+    fileReader.readAsText(file);
+    fileReader.onload = () => {
+      setCode('');
+      setCode(fileReader.result);
     }
   }
 
   return (
-    <div className="Dashboard">
-      <div className='Dashboard-header'>
-        <Button variant="primary" onClick={() => executeCommand(code)} className='Ejecutar'>Ejecutar</Button>
-        <Button variant="secondary" onClick={() => hiddenFileInput.current.click()} className='Abrir'>Abrir Archivo</Button>
-        <Link className='Link-login' to='/login'>
-          <Button variant="success" className='login'>Login</Button>
-        </Link>
-        <input
-          type="file"
-          ref={hiddenFileInput}
-          onChange={handleChange}
-          accept='.mia'
-          style={{ display: 'none' }}
-        />
-      </div>
-      <div className='Title'>
-        <h1>Comandos</h1>
-      </div>
-      <div className='Code'>
-        <MonacoEditor
-          className={'CodeEditor'}
-          language="shell"
-          value={code}
-          options={{ minimap: { enabled: false } }}
-          onChange={setCode}
-        />
-      </div>
-      <div className='Title'>
-        <h1>Consola</h1>
-      </div>
-      <div className='Result'>
-        <MonacoEditor
-          language="shell"
-          theme={'vs-dark'}
-          value={result}
-          options={{ minimap: { enabled: false } }}
-          onChange={setResult}
-        />
-      </div>
+    <div className="App">
+      <header className="App-header">
+      <Navbar bg="secondary" variant='dark' id='navbar'>
+      <Container id='cont-nav'>
+        <Nav id='nav' className="me-auto">
+          <NavDropdown className='navitm' title="Archivo" id="basic-nav-dropdown" ref={navDropdownRef}>
+            <NavDropdown.Item onClick={()=>{open()}}>Crear</NavDropdown.Item>
+            <label id='openinput'>
+                <input
+                  id='fileinput'
+                  type='file'
+                  accept='.tw'
+                  style={{display: 'none'}}
+                  multiple={false}
+                  onChange={(e)=>{
+                    read(e);
+                    navDropdownRef.current.querySelector('.dropdown-menu').classList.remove('show');
+                    document.getElementById('fileinput').value = '';
+                  }}>
+                </input>
+                <span id='openbt'>Abrir</span>
+            </label>
+            <NavDropdown.Item onClick={()=>{save()}}>Guardar</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown className='navitm' title="Reportes" id="basic-nav-dropdown">
+            <NavDropdown.Item onClick={()=>{graphAST()}}>Árbol AST</NavDropdown.Item>
+            <NavDropdown.Item onClick={()=>{generateSymbols()}}>Tabla de Símbolos</NavDropdown.Item>
+            <NavDropdown.Item onClick={()=>{generateErrors()}}>Tabla de Errores</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link className='navitm' onClick={()=>{analizar()} }>Ejecutar</Nav.Link>
+        </Nav>
+      </Container>
+      </Navbar>
+      </header>
+      {/* <div className='Content'> */}
+        <div className="editor1">
+          <Editor
+            height="70vh"
+            theme="vs-dark"
+            defaultLanguage='sql'
+            value={code}
+            options={options}
+            onChange={setCode}
+          />
+        </div>
+      {/* </div> */}
     </div>
   );
 }
 
-export default Dashboard;
+export default App;
