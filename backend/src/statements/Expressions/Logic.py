26e2bc82c519182e98514ae4681fe65f1b2173cc
brandonT2002 <brandonteja02@gmail.com>
2023-12-16 17:15:11 -0600

    [Create] logical operators

---
+++ backend/src/statements/Expressions/Logic.py
@@ -0,0 +1,59 @@
+from statements.Objects.Table import Field
+from utils.Type import ReturnType, Type
+from utils.TypeExp import TypeExp
+from statements.Env.AST import AST, ReturnAST
+from statements.Abstracts.Expression import Expression
+from statements.Env.Env import Env
+
+class Logic(Expression):
+    def __init__(self, line: int, column: int, exp1: Expression, sign: str, exp2: Expression):
+        super().__init__(line, column, TypeExp.NATIVE_FUNC)
+        self.exp1 = exp1
+        self.sign = sign
+        self.exp2 = exp2
+
+    def setField(self, field: dict[str, Field]) -> any:
+        if self.exp1:
+            self.exp1.setField(field)
+        self.exp2.setField(field)
+
+    def execute(self, env: Env) -> ReturnType:
+        match self.sign.upper():
+            case '&&':
+                return self.and_(env)
+            case '||':
+                return self.or_(env)
+            case '!':
+                return self.not_(env)
+            case _:
+                return ReturnType('NULL', Type.NULL)
+
+    def and_(self, env: Env) -> ReturnType:
+        value1: ReturnType = self.exp1.execute(env)
+        value2: ReturnType = self.exp2.execute(env)
+        self.type = Type.BOOLEAN
+        return ReturnType(value1.value and value2.value, self.type)
+
+    def or_(self, env: Env) -> ReturnType:
+        value1: ReturnType = self.exp1.execute(env)
+        value2: ReturnType = self.exp2.execute(env)
+        self.type = Type.BOOLEAN
+        return ReturnType(value1.value or value2.value, self.type)
+
+    def not_(self, env: Env) -> ReturnType:
+        value: ReturnType = self.exp2.execute(env)
+        self.type = Type.BOOLEAN
+        return ReturnType(not value.value, self.type)
+
+    def ast(self, ast: AST) -> ReturnAST:
+        id = ast.getNewID()
+        dot = f'node_{id}[label="{self.sign}"];'
+        value1: ReturnAST
+        if self.exp1 != None:
+            value1 = self.exp1.ast(ast)
+            dot += '\n' + value1.dot
+            dot += f'\nnode_{id} -> node_{value1.id};'
+        value2: ReturnAST = self.exp2.ast(ast)
+        dot += '\n' + value2.dot
+        dot += f'\nnode_{id} -> node_{value2.id};'
+        return ReturnType(dot, id)
