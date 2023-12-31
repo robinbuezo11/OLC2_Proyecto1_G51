1985e78946d65c0882a4138782e735426de3f135
brandonT2002 <brandonteja02@gmail.com>
2023-12-31 13:17:29 -0600

    [update] c3d

--- backend/src/statements/Expressions/Arithmetic.py
+++ backend/src/statements/Expressions/Arithmetic.py
@@ -113,14 +113,14 @@ class Arithmetic(Expression):
         match self.sign:
             case '+':
                 return self.plus_c3d(env, c3dgen)
-            # case '-':
-            #     if self.exp1 != None:
-            #         return self.minus_c3d(env)
-            #     return self.negative_c3d(env)
-            # case '*':
-            #     return self.mult_c3d(env)
-            # case '/':
-            #     return self.div_c3d(env)
+            case '-':
+                if self.exp1 != None:
+                    return self.minus_c3d(env, c3dgen)
+                return self.negative_c3d(env, c3dgen)
+            case '*':
+                return self.mult_c3d(env, c3dgen)
+            case '/':
+                return self.div_c3d(env, c3dgen)
             case _:
                 return ReturnType('NULL', Type.NULL)
 
@@ -193,6 +193,134 @@ class Arithmetic(Expression):
                 return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
         env.setError('Los tipos no son válidos para operaciones aritméticas', self.exp2.line, self.exp2.column)
         return ReturnC3D(isTmp = False, type = self.type)
+
+    def minus_c3d(self, env: Env, c3dgen: C3DGen) -> ReturnC3D:
+        value1: ReturnC3D = self.exp1.compile(env, c3dgen)
+        value2: ReturnC3D = self.exp2.compile(env, c3dgen)
+        self.type = plus[value1.type.value][value2.type.value]
+        if self.type != Type.NULL:
+            if self.type in [Type.INT, Type.DECIMAL]:
+                tmp: str = c3dgen.newTmp()
+                c3dgen.addExpression(tmp, value1.strValue, '-', value2.strValue)
+                return ReturnC3D(isTmp = True, strValue = tmp, type = self.type)
+        env.setError('Los tipos no son válidos para operaciones aritméticas', self.exp2.line, self.exp2.column)
+        return ReturnC3D(isTmp = False, type = self.type)
+
+    def negative_c3d(self, env: Env, c3dgen: C3DGen) -> ReturnC3D:
+        value: ReturnC3D = self.exp2.compile(env, c3dgen)
+        self.type = value.type
+        if self.type in [Type.INT, Type.DECIMAL]:
+            tmp: str = c3dgen.newTmp()
+            c3dgen.addAsign(tmp, '-' + value.strValue)
+            return ReturnC3D(isTmp = True, strValue = tmp, type = self.type)
+        env.setError('Los tipos no son válidos para operaciones aritméticas', self.exp2.line, self.exp2.column)
+        return ReturnC3D(newTmp = False, type = self.type)
+
+    def mult_c3d(self, env: Env, c3dgen: C3DGen) -> ReturnC3D:
+        value1: ReturnC3D = self.exp1.compile(env, c3dgen)
+        value2: ReturnC3D = self.exp2.compile(env, c3dgen)
+        self.type = mult[value1.type.value][value2.type.value]
+        if self.type == Type.BIT:
+                tmp: str = c3dgen.newTmp()
+                trueLbl: str = c3dgen.newLbl()
+                falseLbl: str = c3dgen.newLbl()
+                outLbl: str = c3dgen.newLbl()
+                c3dgen.addIf(value1.strValue, '==', '1', trueLbl)
+                c3dgen.addIf(value1.strValue, '==', '0', falseLbl)
+                c3dgen.addGoto(falseLbl)
+                c3dgen.addLabel(trueLbl)
+                c3dgen.addAsign(tmp, '1')
+                c3dgen.addGoto(outLbl)
+                c3dgen.addLabel(falseLbl)
+                c3dgen.addAsign(tmp, '0')
+                c3dgen.addLabel(outLbl)
+                return ReturnC3D(isTmp = True, strValue = tmp, type = self.type)
+        elif self.type in [Type.INT, Type.DECIMAL]:
+            tmp: str = c3dgen.newTmp()
+            c3dgen.addExpression(tmp, value1.strValue, '*', value2.strValue)
+            return ReturnC3D(isTmp = True, strValue = tmp, type = self.type)
+        elif self.type == Type.DATE or self.type == Type.DATETIME:
+            c3dgen.generateConcatString()
+            tmp1: str = c3dgen.newTmp()
+            c3dgen.addExpression(tmp1, 'P', '+', str(env.size))
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value1.strValue)
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value2.strValue)
+            c3dgen.newEnv(env.size)
+            c3dgen.addCall('_concatString')
+            tmp2: str = c3dgen.newTmp()
+            c3dgen.addGetStack(tmp2, 'P')
+            c3dgen.prevEnv(env.size)
+            return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
+        elif self.type == Type.NVARCHAR or self.type == Type.NCHAR:
+            c3dgen.generateConcatString()
+            tmp1: str = c3dgen.newTmp()
+            c3dgen.addExpression(tmp1, 'P', '+', str(env.size))
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value1.strValue)
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value2.strValue)
+            c3dgen.newEnv(env.size)
+            c3dgen.addCall('_concatString')
+            tmp2: str = c3dgen.newTmp()
+            c3dgen.addGetStack(tmp2, 'P')
+            c3dgen.prevEnv(env.size)
+            return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
+        env.setError('Los tipos no son válidos para operaciones aritméticas', self.exp2.line, self.exp2.column)
+        return ReturnC3D(isTmp = False, type = self.type)
+
+    def div_c3d(self, env: Env, c3dgen: C3DGen) -> C3DGen:
+        value1: ReturnC3D = self.exp1.compile(env, c3dgen)
+        value2: ReturnC3D = self.exp2.compile(env, c3dgen)
+        self.type = div[value1.type.value][value2.type.value]
+        if self.type in [Type.INT, Type.DECIMAL]:
+            if int(value2.strValue) != 0:
+                tmp1: str = c3dgen.newTmp()
+                tmp2: str = c3dgen.newTmp()
+                lbl1: str = c3dgen.newLbl()
+                lbl2: str = c3dgen.newLbl()
+                c3dgen.addAsign(tmp1, value2.strValue)
+                c3dgen.addIf(tmp1, '!=', '0', lbl1)
+                c3dgen.addPrint('MathError: No se puede dividir entre 0\n')
+                c3dgen.addAsign(tmp2, '0')
+                c3dgen.addGoto(lbl2)
+                c3dgen.addLabel(lbl1)
+                c3dgen.addExpression(tmp2, value1.strValue, '/', tmp1)
+                c3dgen.addLabel(lbl2)
+                return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
+            env.setError('No se puede dividir entre 0', self.exp2.line, self.exp2.column)
+            return
+        elif self.type == Type.DATE or self.type == Type.DATETIME:
+            c3dgen.generateConcatString()
+            tmp1: str = c3dgen.newTmp()
+            c3dgen.addExpression(tmp1, 'P', '+', str(env.size))
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value1.strValue)
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value2.strValue)
+            c3dgen.newEnv(env.size)
+            c3dgen.addCall('_concatString')
+            tmp2: str = c3dgen.newTmp()
+            c3dgen.addGetStack(tmp2, 'P')
+            c3dgen.prevEnv(env.size)
+            return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
+        elif self.type == Type.NVARCHAR or self.type == Type.NCHAR:
+            c3dgen.generateConcatString()
+            tmp1: str = c3dgen.newTmp()
+            c3dgen.addExpression(tmp1, 'P', '+', str(env.size))
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value1.strValue)
+            c3dgen.addExpression(tmp1, tmp1, '+', '1')
+            c3dgen.addSetStack(tmp1, value2.strValue)
+            c3dgen.newEnv(env.size)
+            c3dgen.addCall('_concatString')
+            tmp2: str = c3dgen.newTmp()
+            c3dgen.addGetStack(tmp2, 'P')
+            c3dgen.prevEnv(env.size)
+            return ReturnC3D(isTmp = True, strValue = tmp2, type = self.type)
+        env.setError('Los tipos no son válidos para operaciones aritméticas', self.exp2.line, self.exp2.column)
+        return ReturnType('NULL', self.type)
 
     def ast(self, ast: AST) -> ReturnAST:
         id = ast.getNewID()
